f1ad3d0d7bbb118b75a036b0ddbdb619e49ba5c6
onm <omhellyamrut@gmail.com>
2025-09-24 09:40:28 -0600

    start

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
 
 public class Main {
+
+    private void init() {
+    // Initializor
+    GLFWErrorCallback.createPrint(System.err).set();
+}
     public static void main(String[] args) {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
     }
